7791ebc9f88c2bf63d78d5b6ada1dd93a9ac6c78
Kalan225 <kalanthegreatone@users.noreply.github.com>
2020-10-29 17:18:22 +0100

    kolejny commit

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -6,30 +6,36 @@ using System.Threading.Tasks;
 
 namespace Cw1
 {
-    class Program
+    public class Program
     {
         public static async Task Main(string[] args)
         {
-            var url = "https://www.pja.edu.pl";
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(url);
-
-            if (response.IsSuccessStatusCode)
+            
+            var url = args.Length > 0 ? args[0] : "http://www.pja.edu.pl";
+            
+            using (var httpClient = new HttpClient())
             {
-                var htmlContent = await response.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
-                var matches = regex.Matches(htmlContent);
-
-                foreach (var match in matches)
+                using (var response = await httpClient.GetAsync(url))
                 {
-                    Console.WriteLine(match.ToString());
+
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var htmlContent = await response.Content.ReadAsStringAsync();
+                        var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                        var matches = regex.Matches(htmlContent);
+
+                        foreach (var match in matches)
+                        {
+                            Console.WriteLine(match.ToString());
+                        }
+                    }
+
                 }
+                httpClient.Dispose();
             }
-
-
-
         }
     }
+
 }
-
-
