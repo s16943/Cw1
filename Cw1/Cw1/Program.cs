3e8ec268d4893851e135376df54747fc5927747e
Kalan225 <kalanthegreatone@users.noreply.github.com>
2020-10-29 17:17:52 +0100

    kolejny commit

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+
+
+
         }
     }
 }
+
+
