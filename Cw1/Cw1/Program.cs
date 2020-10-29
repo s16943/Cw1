c4ad1ca3a800161c3cf7ab215cc2e492bfc1720a
Kalan225 <kalanthegreatone@users.noreply.github.com>
2020-10-29 17:24:16 +0100

    dodatkowe wymagania

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,24 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Cw1
+namespace cw1
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            
-            var url = args.Length > 0 ? args[0] : "http://www.pja.edu.pl";
-            
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl/component/k2/wazne-kontakty";
+
+            bool result = Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (result)
+            {
+                await GetHttpASyncAsync(url);
+            }
+            else
+            {
+                throw new ArgumentException("Wrong url");
+            }
+
+
+        }
+
+        private static async Task GetHttpASyncAsync(string url)
+        {
             using (var httpClient = new HttpClient())
             {
                 using (var response = await httpClient.GetAsync(url))
                 {
-
-
                     if (response.IsSuccessStatusCode)
                     {
                         var htmlContent = await response.Content.ReadAsStringAsync();
@@ -26,16 +40,38 @@ namespace Cw1
 
                         var matches = regex.Matches(htmlContent);
 
-                        foreach (var match in matches)
+                        if (matches.Count == 0)
                         {
-                            Console.WriteLine(match.ToString());
+                            Console.WriteLine("Nie znaleziono żadnych adresów e-mail");
                         }
+                        else
+                        {
+                            Console.WriteLine("Found e-mails: " + matches.Count);
+                            Hashtable hash = new Hashtable();
+                            foreach (var match in matches)
+                            {
+                                var nextString = match.ToString();
+                                if (hash.Contains(nextString) == false)
+                                {
+                                    hash.Add(nextString, string.Empty);
+                                }
+
+                            }
+                            foreach (DictionaryEntry found in hash)
+                            {
+                                Console.WriteLine(found.Key);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Błąd w czasie pobierania strony");
                     }
 
                 }
                 httpClient.Dispose();
             }
+
         }
     }
-
 }
