ef2a2072d5ddbc46f55d225fd7f0638d3274abfb
Kalan225 <kalanthegreatone@users.noreply.github.com>
2020-10-31 17:58:52 +0100

    poprawki

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -10,32 +10,24 @@ namespace Cw1
     {
         public static async Task Main(string[] args)
         {
-            
+
             var url = args.Length > 0 ? args[0] : "http://www.pja.edu.pl";
-            
-            using (var httpClient = new HttpClient())
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
             {
-                using (var response = await httpClient.GetAsync(url))
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var match in matches)
                 {
-
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var htmlContent = await response.Content.ReadAsStringAsync();
-                        var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                        var matches = regex.Matches(htmlContent);
-
-                        foreach (var match in matches)
-                        {
-                            Console.WriteLine(match.ToString());
-                        }
-                    }
-
+                    Console.WriteLine(match.ToString());
                 }
-                httpClient.Dispose();
             }
-        }
-    }
 
+        }
+
+    }
 }
