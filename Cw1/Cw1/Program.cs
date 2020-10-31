cdfe1131004e8d82d3f23a3dd8c97e73a1e89bd2
Kalan225 <kalanthegreatone@users.noreply.github.com>
2020-10-31 18:43:17 +0100

    popraweczki

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,19 +10,21 @@ namespace cw1
     {
         public static async Task Main(string[] args)
         {
-            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl/component/k2/wazne-kontakty";
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
 
             bool result = Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult)
                 && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+
             if (result)
             {
                 await GetHttpASyncAsync(url);
             }
             else
             {
-                throw new ArgumentException("Wrong url");
+                throw new ArgumentException("Zły adres URL!!");
             }
-
+            
 
         }
 
@@ -31,8 +32,8 @@ namespace cw1
         {
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync(url))
-                {
+                var response = await httpClient.GetAsync(url);
+                
                     if (response.IsSuccessStatusCode)
                     {
                         var htmlContent = await response.Content.ReadAsStringAsync();
@@ -46,7 +47,7 @@ namespace cw1
                         }
                         else
                         {
-                            Console.WriteLine("Found e-mails: " + matches.Count);
+                            Console.WriteLine("Znalezione adresyy: " + matches.Count);
                             Hashtable hash = new Hashtable();
                             foreach (var match in matches)
                             {
@@ -65,10 +66,10 @@ namespace cw1
                     }
                     else
                     {
-                        Console.WriteLine("Błąd w czasie pobierania strony");
+                        Console.WriteLine("Błąd pobierania strony");
                     }
 
-                }
+                // prawidłowe zwalnianie zasobów
                 httpClient.Dispose();
             }
 
